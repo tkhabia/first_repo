a32e057baa1d427453f0020c04817766c65364f2
tkhabia <tanmay.khabia@students.iiit.ac.in>
2018-11-07 15:14:46 +0530

    version2

--- elect1.c
+++ elect1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
     long long int x[1030000]; 
-    long long int y[1030000],eq, xs[1003000], ys[1003000], temp ,temp2/*,xe[1000300]={0},ye[1000030]={0}*/ ;
+    long long int y[1030000],eqx[1003000],eqy[1003000], xs[1003000], ys[1003000], temp ,temp2,eq;
     long long int L[1030000], R[1030000],L1[1030000], R1[1030000];
 void merge(long long int x[], long long int y[], long long int l, long long int m, long long int r) 
 { 
@@ -125,31 +125,41 @@ void mergeSort2(long long int x[],long long int y[], long long int l, long long 
         mergeSort2(x,y, m+1, r);   
         merge2(x,y, l, m, r); 
     } 
-}  
-// long long int mul2(long long int n, long long int m, long long int mod){
+} 
+long long int mul2(long long int n, long long int m, long long int mod){
     
-//     long long int temp = m % mod;
-//     long long int ans2 =0;
-//             while(temp){
-//                 if(temp&1)
-//                     ans2 = (ans2 + n)%mod;
-//                 n = (n*2)%mod;
-//                 temp /=2;
-//             }
-//             return ans2;
-// }
-    long long int C[1002000]; 
-long long int ncr(long long int n ,long long int r,long long int mod){
-  
-    C[0] = 1;  
-  
-    for (long long int i = 1; i <= n; i++) 
-         for (int j =i>r?r:i ; j > 0; j--) 
-             C[j] = (C[j] + C[j-1])%mod; 
-    
-    return C[r];
+    long long int temp = m % mod;
+    long long int ans2 =0;
+            while(temp){
+                if(temp&1)
+                    ans2 = (ans2 + n)%mod;
+                n = (n*2)%mod;
+                temp /=2;
+            }
+            return ans2;
 }
-    
+long long int ncr(long long int n,long long int mod){
+    if(n==3)
+        return 1;
+    else if(n%3== 0){
+        if(n-1%2==0)
+            return mul2(n/3,mul2((n-1)/2,n-2,mod), mod);
+        else
+            return mul2(n/3,mul2((n-1),(n-2)/2,mod), mod);
+
+    }else if(n-1%3){
+        if(n%2==0)
+            return mul2(n/2,mul2((n-1)/3,n-2,mod), mod);
+        else
+            return mul2(n,mul2((n-1)/3,(n-2)/2,mod), mod);
+    }else {
+        if(n%2==0)
+            return mul2(n/2,mul2((n-1),(n-2)/3,mod), mod);
+        else
+            return mul2(n,mul2((n-1)/2,(n-2)/3,mod), mod);
+    }
+} 
+
 int main() 
 { 
     long long int n, mod, i;
@@ -161,26 +171,38 @@ int main()
     mergeSort(x,y,0,n-1); 
   // printArray(x,y,n);  
     long long int sum = 0, sum2= 0,ans = 0;
-    for (int i = 2; i < n; ++i)
+    //temp = 0;
+    for (int i = 1; i < n; ++i)
     {
         if(x[i]==x[i-1]&&y[i]==y[i-1]){
-            eq++;
-        }else if(x[i]== x[i-1]){
-            if(i!= n-1 && x[i]==x[i+1] && y[i]!= y[i+1])
+        
+            eqx[temp]++;
+        } 
+        if(x[i]== x[i-1]){
+        
+            //if(i!= n-1 && x[i]==x[i+1] && y[i]!= y[i+1])
+        
                 xs[temp]++;
+        
         }else{
+        
             if(xs[temp]){
+        
                 temp++;
             }
         }
     }
+     if(xs[temp]){
+                temp++;
+            }
+
     mergeSort2(x,y,0,n-1);
-   // printArray(x,y,n);
     for (int i = 1; i < n; ++i)
     {
         if(x[i]==x[i-1]&&y[i]==y[i-1]){
-             continue;
-        }else if(y[i]== y[i-1]&&i!= n-1&&y[i]==y[i+1]&&x[i]!= x[i+1]){
+             eqy[temp2]++;
+        }
+        if(y[i]== y[i-1] /*&& i!= n-1&&y[i]==y[i+1]&&x[i]!= x[i+1]*/){
               ys[temp2]++;
         }else{
             if(ys[temp2]){
@@ -188,15 +210,20 @@ int main()
             }
         }
     }
+    if(ys[temp2]){
+                temp2++;
+            }
     for(i= 0;i< temp;i++){
-        sum = (sum + ncr(xs[i] + 2,3,mod))%mod;
+        sum = (sum + ncr(xs[i]-eqx[i] + 1,mod))%mod;
+        // printf("%lld", ncr(xs[i] + 2,mod));
     }
     for (int i = 0; i < temp2; ++i)
     {
-        sum2 = (sum2+ncr(ys[i] + 2,3,mod))%mod;
+        eq += eqy[i];
+        sum2 = (sum2+ncr(ys[i] -eqy[i]+1 ,mod))%mod;
     }
-    ans = (ncr(n-eq,3,mod)-(sum+sum2)%mod)%mod;
+    ans = (ncr(n-eq,mod)-(sum+sum2)%mod)%mod;
+    printf("ys=%lld xs=%lld eq=%lld ncr %Ld sum2 %Ld sum %lld \n",ys[0], xs[0],eq,ncr(n-eq,mod),sum2,sum);
     printf("%Ld\n",ans);
-//printf("ys=%lld xs=%lld eq=%lld\n",ys[0], xs[0],eq);
     return 0; 
 }
